710af25366fc26954021056af80ec45ef2dfc27f
IDontCaramel <lucaskoot@icloud.com>
2024-03-13 09:28:33 +0100

    gun now shooting

--- top down shooter/Assets/scripts/Movement.cs
+++ top down shooter/Assets/scripts/Movement.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -6,12 +7,24 @@ public class Movement : MonoBehaviour
     public float speed = 8f;
     public Rigidbody2D rb;
     public Transform gun;
+    public Transform ShootPoint;
+    public float bulletSpeed;
+    public GameObject bullet;
+
+    public KeyCode FireKey;
 
     Vector2 moveDirection;
     Vector2 mousePosition;
+    private float aimAngle;
 
     private void Update()
     {
+        while (Input.GetMouseButtonDown(0) || Input.GetKeyDown(FireKey))
+        {
+            
+        }
+
+
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
@@ -27,4 +40,21 @@ public class Movement : MonoBehaviour
         float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
         gun.rotation = Quaternion.Euler(0f, 0f, aimAngle);
     }
+
+    public void ShootGun()
+    {
+        GameObject spawnedObject = Instantiate(bullet, ShootPoint.position, Quaternion.identity);
+
+        Rigidbody2D bulletRb = spawnedObject.GetComponent<Rigidbody2D>();
+
+        if (bulletRb != null)
+        {
+            bulletRb.velocity = new Vector2(Mathf.Cos(aimAngle * Mathf.Deg2Rad), Mathf.Sin(aimAngle * Mathf.Deg2Rad)) * bulletSpeed;
+        }
+        else
+        {
+            Debug.LogWarning("Rigidbody2D component not found on the prefab. Velocity not applied.");
+        }
+    }
+
 }
