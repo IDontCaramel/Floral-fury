506b2620313c2c80fe945c8336f58f14fa4b5739
Cas Veldhuizen <casvld@outlook.com>
2024-03-15 12:44:47 +0100

    allemaal prefabs

--- top down shooter/Assets/scripts/Enemy.cs
+++ top down shooter/Assets/scripts/Enemy.cs
@@ -62,30 +62,18 @@ public class Enemy : MonoBehaviour
         if (player != null)
         {
             Vector2 directionToPlayer = player.transform.position - transform.position;
-            Debug.DrawRay(transform.position, directionToPlayer, Color.cyan);
 
             if (directionToPlayer.magnitude <= visionRange)
             {
                 RaycastHit2D ray = Physics2D.Raycast(transform.position, directionToPlayer, visionRange);
 
-                if (ray.collider != null)
-                {
-                    Debug.Log("Ray hit: " + ray.collider.gameObject.name);
-                }
-                else
-                {
-                    Debug.Log("Ray did not hit anything.");
-                }
-
                 if (ray.collider != null && ray.collider.gameObject == player)
                 {
-                    Debug.Log("player found");
                     isPatrol = false;
                     Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
                 }
                 else
                 {
-                    Debug.Log("player not found");
                     isPatrol = true;
                     Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
                 }
@@ -93,7 +81,6 @@ public class Enemy : MonoBehaviour
             }
             else
             {
-                Debug.Log("player exits idk");
                 isPatrol = true;
             }
         }
@@ -107,7 +94,7 @@ public class Enemy : MonoBehaviour
         if (health <= 0)
         {
             GameObject instance = Instantiate(scorePopUp, transform.position, Quaternion.identity);
-            target.GetComponent<Player>().Points += PointsWorth;
+            target.GetComponent<Player>().AddPoints(PointsWorth);
             instance.GetComponentInChildren<TextMeshProUGUI>().text = "+" + PointsWorth;
             Instantiate(deathEffect, transform.position, Quaternion.identity);
             Destroy(gameObject);
